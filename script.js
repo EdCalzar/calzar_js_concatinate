c3c0c6d9ac203c3f78cbbfe0a30f35f5e6924e29
EdCalzar <146654155+EdCalzar@users.noreply.github.com>
2024-05-17 23:09:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,187 @@
+let contentDiv = document.getElementById("content");
+contentDiv.classList.add("custom-font");
+
+let title = "PROGRAMMER'S POEM";
+
+// Create title element
+let titleElement = document.createElement("h1");
+titleElement.textContent = title;
+titleElement.classList.add("title"); // Add a class to the title element
+contentDiv.appendChild(titleElement);
+
+let odeToRod_theWebWeaver = "<b># Ode to Rod: The Web Weaver<b>";
+let realm = "<b>realm</b>";
+let pixels = "<b>pixels</b>";
+let code = "<b>code</b>";
+let webWeaver = "<b>**web weaver**</b>";
+let pearls = "<b>pearls</b>";
+let rod2 = "<i>Rod</i>"
+
+let blankCanvas = "<b>I. The Blank Canvas</b>";
+let rod = "<b>Rod</b>";
+let alchemistOfTheBrowser = "<b>**alchemist of the browser**</b>";
+let keyboard = "<b>keyboard</b>";
+let wand = "<b>wand</b>";
+let pages = "<b>pages</b>";
+let painting = "<b>painting</b>";
+
+let htmlSymphony = "<b>II. The HTML Symphony<b>";
+let fingers = "<b>fingers</b>";
+let keys = "<b>keys</b>";
+let tags = "<b>tags</b>";
+let html = "<b>HTML</b>";
+let symphonies = "<b>symphonies</b>";
+
+let cssEnchanter = "<b>III. The CSS Enchanter<b>";
+let cssCloak = "<b>CSS cloak</b>";
+let styling = "<b>Styling</b>";
+let colorsBloom_fontsSing = "<b>Colors bloom, fonts sing,</b>";
+let evoke = "<b>evoke,</b>";
+let visual = "<b>visual</b>";
+
+let javaScriptMagician = "<b>IV. The JavaScript Magician</b>";
+let magic = "<b>magic</b>";
+let javaScriptSpells = "<b>JavaScript spells</b>";
+let will = "<b>will</b>";
+let forms = "<b>forms</b>";
+let animationsPropel = "<b>animations propel</b>.";
+
+let debuggingSage = "<b>V. The Debugging Sage</b>";
+let bugs = "<b>bugs</b>";
+let debuggingSpectacles = "<b>debugging spectacles</b>";
+let glitches = "<b>glitches</b>";
+let codesArc = "<b>code's arc,</b>";
+let tames = "<b>tames</b>";
+let variables = "<b>variables.</b>"
+
+let responsiveVoyager = "<b>VI. The Responsive Voyager</b>";
+let across = "<b>Across</b>";
+let devices = "<b>devices</b>";
+let responsiveSea = "<b>responsive sea</b>";
+let guide = "<b>guide</b>";
+let adapting = "<b>Adapting</b>";
+let fluidity = "<b>fluidity</b>";
+
+let seoWhisperer = "<b>## VII. The SEO Whisperer</b>";
+let labyrinth = "<b>labyrinth</b>";
+let seoWhispererTerm = "<b>SEO whisperer</b>";
+let metaTags = "<b>Meta tags</b>";
+let wanderers = "<b>wanderers.</b>";
+
+let gitAlchemist = "<b>VIII. The Git Alchemist</b>";
+let mysticalPotion = "<b>mystical potion,</b>";
+let git = "<b>Git's</b>";
+let merge = "<b>merge</b>";
+let repository = "<b>repository</b>";
+
+let deploymentDreamer = "<b>IX. The Deployment Dreamer<b>";
+let moon = "<b>moon</b>";
+let world = "<b>world</b>";
+let deployments = "<b>Deployments</b>";
+let website = "<b>website</b>";
+let unfurled = "<b>unfurled</b>";
+
+let webDeveloperAnthem = "<b>X. The Web Developer's Anthem<b>";
+let webDeveloper = "<b>web developer</b>";
+let clicks = "<b>clicks</b>";
+let binary = "<b>binary</b>";
+let matrix = "<b>matrix</b>";
+
+let may = "<i>may</i>";
+let your = "<i>your</i>";
+let lines = "<i>lines</i>";
+let be = "<i>be</i>";
+let bugFree = "<i>bug-free</i>";
+let may2 = "<i>May</i>";
+let css = "<i>CSS</i>";
+let always = "<i>always</i>";
+let cascade = "<i>cascade</i>";
+let gracefully = "<i>gracefully</i>";
+let and = "<i>And</i>";
+let javascript = "<i>JavaScript</i>";
+let forever = "<i>forever</i>";
+let flow = "<i>flow</i>";
+let as1 = "<i>As</i>";
+let you = "<i>you</i>";
+let navigate = "<i>navigate</i>";
+let the = "<i>the</i>";
+let web = "<i>web's</i>";
+let eternal = "<i>eternal</i>";
+let ebb = "<i>ebb</i>";
+let and2 = "<i>and</i>";
+
+contentDiv.innerHTML += odeToRod_theWebWeaver + "<br>";
+contentDiv.innerHTML += "In the " + realm + " of ones and zeros," + "<br>";
+contentDiv.innerHTML += "Where " + pixels + " dance and " + code + " unfurls," + "<br>";
+contentDiv.innerHTML += "There resides a silent sorcerer," + "<br>";
+contentDiv.innerHTML += "A " + webWeaver + " with dreams and " + pearls + "." + "<br>";
+contentDiv.innerHTML += "<br>";
+
+contentDiv.innerHTML += blankCanvas + "<br>";
+contentDiv.innerHTML += rod + ", " + alchemistOfTheBrowser + "," + "<br>";
+contentDiv.innerHTML += "Wields his " + keyboard + " like a " + wand + "," + "<br>";
+contentDiv.innerHTML += "He conjures " + pages + " from thin air," + "<br>";
+contentDiv.innerHTML += painting + " landscapes with lines so fond." + "<br>";
+contentDiv.innerHTML += "<br>";
+contentDiv.innerHTML += htmlSymphony + "<br>";
+contentDiv.innerHTML += "His " + fingers + " dance upon the " + keys + "<br>";
+contentDiv.innerHTML += "Crafting " + tags + " like musical notes," + "<br>";
+contentDiv.innerHTML += html + " whispers secrets to the breeze," + "<br>";
+contentDiv.innerHTML += "As " + rod + " orchestrates " + symphonies + " in quotes." + "<br>";
+contentDiv.innerHTML += "<br>";
+
+contentDiv.innerHTML += cssEnchanter + "<br>";
+contentDiv.innerHTML += "Next, he dons his **" + cssCloak + "**," + "<br>";
+contentDiv.innerHTML += styling + "elements with flair and grace," + "<br>";
+contentDiv.innerHTML += colorsBloom_fontsSing + " layouts " + evoke + "<br>";
+contentDiv.innerHTML += "A" + visual + "ballet in this digital space." + "<br>";
+contentDiv.innerHTML += "<br>";
+
+contentDiv.innerHTML += javaScriptMagician + "<br>";
+contentDiv.innerHTML += "But wait, the " + magic + " deepens still," + "<br>";
+contentDiv.innerHTML += rod + " summons **" + javaScriptSpells + "**," + "<br>";
+contentDiv.innerHTML += "Interactivity blooms at his" + will + "," + "<br>";
+contentDiv.innerHTML += forms + " validate, " + animationsPropel + "<br>";
+contentDiv.innerHTML += "<br>";
+
+contentDiv.innerHTML += debuggingSage + "<br>";
+contentDiv.innerHTML += "Yet," + bugs + "creep in like shadows dark," + "<br>";
+contentDiv.innerHTML += rod + " dons his **" + debuggingSpectacles + "**," + "<br>";
+contentDiv.innerHTML += "Chasing " + glitches + " through the " + codesArc + "<br>";
+contentDiv.innerHTML += "He " + tames + " wild loops and elusive " + variables + "<br>";
+contentDiv.innerHTML += "<br>";
+
+contentDiv.innerHTML += responsiveVoyager + "<br>";
+contentDiv.innerHTML += across + " screens," + devices + ", and dimensions," + "<br>";
+contentDiv.innerHTML += rod + " sails on the **" + responsiveSea + "**," + "<br>";
+contentDiv.innerHTML += "Media queries " + guide + " his intentions," + "<br>";
+contentDiv.innerHTML += adapting + " layouts with " + fluidity + "." + "<br>";
+contentDiv.innerHTML += "<br>";
+
+contentDiv.innerHTML += seoWhisperer + "<br>" + "In the " + labyrinth + " of search engines," + "<br>" + rod + " becomes an **" + seoWhispererTerm + "**," + "<br>"
+    + metaTags + ", " + "keywords, backlink strings," + "<br>" + "He crafts pathways to digital " + wanderers + "<br>";
+
+contentDiv.innerHTML += "<br>";    
+
+contentDiv.innerHTML += gitAlchemist + "<br>";
+contentDiv.innerHTML += "Version control, a " + mysticalPotion + "," + "<br>" + rod + " brews commits in " + git + " cauldron," + "<br>" + " Branches fork, " +
+    merge + " conflicts in motion," + "<br>" + "His " + repository + " a saga of " + code + " reborn." + "<br>";
+
+contentDiv.innerHTML += "<br>"; 
+
+contentDiv.innerHTML += deploymentDreamer + "<br>";
+contentDiv.innerHTML += "And when the " + moon + " kisses the horizon," + "<br>" + rod + " unfurls his creation to the " + world + "," + "<br>" + deployments +
+    " like shooting stars, arisen," + "<br>" + "His " + website + "sails, " + "its sails " + unfurled + "." + "<br>";
+
+contentDiv.innerHTML += "<br>";
+
+contentDiv.innerHTML += webDeveloperAnthem + "<br>";
+contentDiv.innerHTML += "So here's to " + rod + ", the **" + webDeveloper + "**," + "<br>" + "A poet of " + pixels + ", a composer of " + clicks + "," + "<br>" +
+    "In the " + binary + " symphony, he's the reveler," + "<br>" + "Weaving dreams into the digital " + matrix + "." + "<br>";
+
+contentDiv.innerHTML += "<br>";
+
+contentDiv.innerHTML += "*" + rod2 + ", " + may + " " + your + " " + lines + " " + be + " " + bugFree + "," + "<br>" + may2 + " " + your + " " + css + " " + 
+    always + " " + cascade + " " + gracefully + ", " + "<br>" + and + " " + may + " " + your + " " + javascript + " " + forever + " " + flow + "," + "<br>" + 
+    as1 + " " + you + " " + navigate + " " + web + " " + eternal + " " + ebb + " " + and2 + " " + flow + ".*";
+
